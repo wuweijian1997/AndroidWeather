32ac8e2a32a72dbecead19d47e9b0e7657d2e642
kafka <kafka@yunio.com>
2021-01-20 18:16:09 +0800

    插件版本更新

--- app/src/main/java/com/logic/weather/WeatherActivity.kt
+++ app/src/main/java/com/logic/weather/WeatherActivity.kt
@@ -13,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.logic.weather.databinding.ActivityWeatherBinding
 import com.logic.weather.logic.model.Weather
 import com.logic.weather.ui.weather.WeatherViewModel
@@ -24,21 +24,14 @@ import java.util.*
 class WeatherActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityWeatherBinding
-    val viewModel by lazy { ViewModelProviders.of(this).get(WeatherViewModel::class.java) }
+    val viewModel by lazy { ViewModelProvider(this)[WeatherViewModel::class.java] }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         ///沉浸屏
-        /*val decorView = window.decorView
+        val decorView = window.decorView
         decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.statusBarColor = Color.TRANSPARENT
-        }*/
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.statusBarColor = Color.TRANSPARENT
-        }
-
 
         binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -85,7 +78,10 @@ class WeatherActivity : AppCompatActivity() {
             override fun onDrawerClosed(drawerView: View) {
                 val manager = getSystemService(Context.INPUT_METHOD_SERVICE)
                         as InputMethodManager
-                manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+                manager.hideSoftInputFromWindow(
+                    drawerView.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
             }
 
             override fun onDrawerOpened(drawerView: View) {
@@ -160,9 +156,9 @@ class WeatherActivity : AppCompatActivity() {
     companion object {
         fun open(context: Context, locationLng: String, locationLat: String, placeName: String) {
             val intent = Intent(context, WeatherActivity::class.java)
-            intent.putExtra("location_lng",locationLng)
-            intent.putExtra("location_lat",locationLat)
-            intent.putExtra("place_name",placeName)
+            intent.putExtra("location_lng", locationLng)
+            intent.putExtra("location_lat", locationLat)
+            intent.putExtra("place_name", placeName)
             context.startActivity(intent)
         }
     }
